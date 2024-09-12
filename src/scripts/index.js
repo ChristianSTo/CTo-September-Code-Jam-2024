d71fbed473ac0aebd0f9330e8dad62caff73c587
Elvinish <Elvinish64@gmail.com>
2024-09-12 11:58:14 -0700

    test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,7 @@
 import "../pages/index.css";
+
+// function closeModal(modal) {
+//   modal.classList.remove("modal_opened");
+//   document.removeEventListener("keydown", closeModalOnEvent);
+//   document.removeEventListener("click", closeModalOnEvent);
+// }
