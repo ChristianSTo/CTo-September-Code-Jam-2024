d86d5730e9e861cd5a8d3458829b3233bb1a6fbc
ChristianTo <christian.to@outlook.com>
2024-09-15 16:10:38 -0500

    Updates

--- src/scripts/Nav.js
+++
@@ -1,7 +0,0 @@
-const hamBtn = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav__links");
-
-hamBtn.addEventListener("click", () => {
-  hamBtn.classList.toggle("active");
-  nav.classList.toggle("active");
-});
